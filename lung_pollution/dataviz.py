60906c59f098aded1e5fad77a1ac505100f6d344
Dorien <41797896+dorien-er@users.noreply.github.com>
2021-12-01 12:19:30 +0100

    restore dataviz

--- lung_pollution/dataviz.py
+++ lung_pollution/dataviz.py
@@ -11,20 +11,21 @@ from urllib.request import urlopen
 
 class DataViz():
 
-    def __init__(self, file_name='lung_pollution/data/covid_pollution_final-rifqi.csv'):
+    def __init__(self, file_name='../lung_pollution/data/covid_pollution_json_rifqi.csv'):
         #self.file_name = '../lung_pollution/data/covid_pollution_json_rifqi.csv'
         self.file_name = file_name
 
     def load_data(self):
-        self.df = pd.read_csv(self.file_name)
-        return self.df
+        df = pd.read_csv(self.file_name)
+        return df
 
-    def fig_map(self, *args, **kwargs):
-        url = 'https://raw.githubusercontent.com/isellsoap/deutschlandGeoJSON/main/4_kreise/1_sehr_hoch.geo.json'
-        with urlopen(url) as response:
+
+
+    def fig(self, *args, **kwargs):
+        app = dash.Dash()
+        with urlopen('https://raw.githubusercontent.com/isellsoap/deutschlandGeoJSON/main/4_kreise/1_sehr_hoch.geo.json') as response:
             counties = json.load(response)
-        fig = px.choropleth_mapbox(
-                           self.df,
+        fig = px.choropleth_mapbox(self.df_full_json,
                            geojson=counties,
                            locations='county_new',
                            featureidkey="properties.NAME_3",
@@ -37,8 +38,6 @@ class DataViz():
                            labels={'cases':'cases per 100k'}
                           )
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-        app = dash.Dash()
         app.layout = dbc.Container([
             dbc.Row([
                 dbc.Col(html.Div([html.H1("Lung Pollution", className='display-3'),
@@ -62,12 +61,7 @@ class DataViz():
                 ]), width=10)
             ])
             ], fluid=True)
-
-        return app
-
-if __name__ == '__main__':
-    viz = DataViz()
-    viz.load_data()
-    #print(data)
-    app = viz.fig_map()
-    app.run_server(host='0.0.0.0', port=8070, debug=True, use_reloader=False)
+        if __name__ == '__main__':
+            app.run_server(host='0.0.0.0', port=8089,
+                   debug=True, use_reloader=False)
+        return fig
