704e28a7b5dd01f6dd776cb42a06f844652cfaf7
Dorien <41797896+dorien-er@users.noreply.github.com>
2021-12-01 21:23:09 +0100

    created final api and docker

--- api/fast.py
+++ api/fast.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sklearn.preprocessing import StandardScaler
 
 import pandas as pd
 import joblib
@@ -22,12 +21,12 @@ def index():
     return {"this": "is a test"}
 
 @app.get("/predict")
-def predict(NO,      # 0
-            NO2,       # 0
-            PM2_5,        # 0
-            O3,      # 0
-            vax,       # 0
-            density):       # 1
+def predict(NO,
+            NO2,
+            PM2_5,
+            O3,
+            vax,
+            density):
 
     X_test = pd.DataFrame(dict(NO=[float(NO)],
                                NO2 = [float(NO2)],
@@ -36,12 +35,9 @@ def predict(NO,      # 0
                                vax = [float(vax)],
                                density = [float(density)]))
 
-    scaler = StandardScaler()
-    scaler.fit(X_test)  # Fit scaler to feature
+    scaler = joblib.load('scaler.joblib')
     X_test = scaler.transform(X_test)
-    print(os.getcwd())
     model = joblib.load('model.joblib')
-
     results = model.predict(X_test)
     pred = float(results[0])
     return dict(prediction=pred)
